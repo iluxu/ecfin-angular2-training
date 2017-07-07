c95e3989f0683109119d2e2370ed536482790995
Rafal Rokoszewski <rafbr1@gmail.com>
2017-07-07 13:11:50 +0200

    initial - exercise1

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-root',
+  moduleId: module.id,
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+  public student = 'Han Solo'
+}
